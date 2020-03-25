6803f3adc6300134a50608c32764ff26f76b8e10
Naviforyou <kwforus@gmail.com>
2020-03-25 20:28:52 +0900

    SCM 0325 현위치 버튼 추가 및 클릭시 좌표 반환

--- app/src/main/java/com/example/naviforyou/MainActivity.java
+++ app/src/main/java/com/example/naviforyou/MainActivity.java
@@ -5,16 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.widget.ZoomControlView;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,7 +32,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private FusedLocationSource locationSource;
 
     //마커 객체 생성
-    Marker maker = new Marker();
+    Marker marker = new Marker();
+    InfoWindow infoWindow = new InfoWindow();
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +43,11 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         //NaverMap 객체 얻어오기 - api 호출하는 인터페이스 역할을 함
         FragmentManager fm = getSupportFragmentManager();
-        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_fragment);
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
         if (mapFragment == null) {
             mapFragment = MapFragment.newInstance();
-            fm.beginTransaction().add(R.id.map_fragment, mapFragment).commit();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
         }
-
         mapFragment.getMapAsync(this);
 
         // 위치를 반환하는 FusedLocationSource 선언
@@ -51,20 +61,55 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(@NonNull NaverMap naverMap) {
         // UI 설정
         UiSettings uiSettings = naverMap.getUiSettings();
-        uiSettings.setCompassEnabled(true); // 나침반 활성화 여부
-        uiSettings.setScaleBarEnabled(true); // 축적바
-        uiSettings.setZoomControlEnabled(true); //줌버튼
-        uiSettings.setLocationButtonEnabled(true); //현위치 버튼
 
-        // FusedLocationSource을 NaverMap에 지정정
+        // FusedLocationSource을 NaverMap에 지정
        naverMap.setLocationSource(locationSource);
-       naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+       naverMap.setLocationTrackingMode(LocationTrackingMode.NoFollow);
 
        //위치 오버레이 객체 추가
        LocationOverlay locationOverlay = naverMap.getLocationOverlay();
 
        //오버레이 추가
-        maker.setMap(naverMap);
+
+       marker.setPosition(new LatLng(37.5670135, 126.9783740));
+       marker.setMap(naverMap);
+       infoWindow.open(marker);
+
+       Log.i(this.getClass().getName(), String.valueOf(uiSettings.isCompassEnabled()));
+
+        btn = findViewById(R.id.me);
+
+        //자기 위치로 돌아가는 버튼 데모
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(naverMap.getLocationTrackingMode() == LocationTrackingMode.NoFollow){
+                    naverMap.setLocationTrackingMode(LocationTrackingMode.Face);
+                }else{
+                    naverMap.setLocationTrackingMode(LocationTrackingMode.NoFollow);
+                }
+            }
+        });
+
+        //심벌 클릭
+        naverMap.setOnSymbolClickListener(symbol -> {
+            Toast.makeText(this, symbol.getCaption(), Toast.LENGTH_SHORT).show();
+            return true;
+            /*
+            if ("서울특별시청".equals(symbol.getCaption())) {
+                Toast.makeText(this, "서울시청 클릭", Toast.LENGTH_SHORT).show();
+                // 이벤트 소비, OnMapClick 이벤트는 발생하지 않음
+                return true;
+            }
+            // 이벤트 전파, OnMapClick 이벤트가 발생함
+            return false;
+            */
+        });
+
+        //클릭
+        naverMap.setOnMapClickListener((point, coord) ->
+                Toast.makeText(this, coord.latitude + ", " + coord.longitude,
+                        Toast.LENGTH_SHORT).show());
     }
 
     //위치 권한 허용
