fd276b1c5c71c73fdcd4157dda367be37de82e9c
Irene Lee <49191920+jiulee123123@users.noreply.github.com>
2020-04-25 02:04:51 +0900

    JIU 0425
    - add comment

--- app/src/main/java/com/example/naviforyou/model/FavDB.java
+++ app/src/main/java/com/example/naviforyou/model/FavDB.java
@@ -1,3 +1,4 @@
+// 줄겨찾기 데이터베이스
 // made by Jiu
 // https://www.youtube.com/watch?v=T3y3370UE8w
 
@@ -12,12 +13,12 @@ import android.util.Log;
 
 public class FavDB extends SQLiteOpenHelper {
 
-    private static int DB_VERSION = 1;
+    private static int DB_VERSION = 1; //DB 버전 몇번인지
     private static String DATABASE_NAME = "PlaceDB";
-    private static String TABLE_NAME = "favoriteTable";
+    private static String TABLE_NAME = "favoriteTable"; // SQLite 사용하기에 만드는 표이름
     private static String KEY_ID = "id";
-    public static String PLACE_TITLE = "placeTitle";
-    public static String FAVORITE_STATUS ="fStatus";
+    public static String PLACE_TITLE = "placeTitle"; // 장소 이름
+    public static String FAVORITE_STATUS ="fStatus"; //즐겨찾기에 추가된 상태인지 여부
     private static String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
             + KEY_ID + " TEXT,"+ PLACE_TITLE + " TEXT,"
             + FAVORITE_STATUS + "TEXT)";
