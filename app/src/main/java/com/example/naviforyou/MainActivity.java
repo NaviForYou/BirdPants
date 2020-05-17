6fb7693cb9a5441b5e07ea1b29c34d05f341e29b
mcms0404 <mcms0404@naver.com>
2020-05-17 15:24:35 +0900

    KMC 0507 프래그먼트 수정

--- app/src/main/java/com/example/naviforyou/MainActivity.java
+++ app/src/main/java/com/example/naviforyou/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     Search_Parser search_parser = new Search_Parser();
     LowbusStop_Parser lowbusStop_parser = new LowbusStop_Parser();
     TextView search_bar; //main layout의 검색창
+    private View frame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,9 +81,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
 
-        /*
-        search_bar = findViewById(R.id.search_bar);
-        search_bar.setOnClickListener(new View.OnClickListener() {
+        frame = findViewById(R.id.frame);
+        frame.setOnClickListener(new View.OnClickListener() {
             @Override  //클릭했얼 경우 검책 프래그먼트창 켜지기 근데 이게 프레임 레이아웃이 바뀌는 느낌이어서 드로워 레이아웃 뒤로 가는듯 하닫
             public void onClick(View v) {
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -91,7 +91,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 transaction.commit();
             }
         });
-        */
     }
 
     @Override
