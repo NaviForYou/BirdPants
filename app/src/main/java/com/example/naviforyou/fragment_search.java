09f79c5dcdee72412b3836c4900d322d26812574
mcms0404 <mcms0404@naver.com>
2020-05-18 10:43:28 +0900

    min-branch

---
+++ app/src/main/java/com/example/naviforyou/fragment_search.java
@@ -0,0 +1,25 @@
+package com.example.naviforyou;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class fragment_search extends Fragment {
+
+    public fragment_search() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+}
