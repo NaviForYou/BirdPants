b8b0479bd3fc23ced5f35cfd01fceb4958f24c39
bangdaexp <dnflquf3gh@naver.com>
2020-06-09 02:20:40 +0900

    BDY 0609 try HORIZONTAL recyclerview

--- app/src/main/java/com/example/naviforyou/MainActivity.java
+++ app/src/main/java/com/example/naviforyou/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.naviforyou;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.naviforyou.R;
+
+import java.util.ArrayList;
+
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+    private MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //data to populate the RecyclerView with
+        ArrayList<String> choose_main = new ArrayList<>();
+        choose_main.add("전체");
+        choose_main.add("도보");
+        choose_main.add("버스");
+        choose_main.add("지하철");
+        choose_main.add("버스+지하철");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.way);
+        LinearLayoutManager horizontalLayoutManager
+                = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(horizontalLayoutManager);
+        adapter = new MyRecyclerViewAdapter(this, choose_main);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on item position " + position, Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/com/example/naviforyou/listViewAdapter.java
+++ app/src/main/java/com/example/naviforyou/listViewAdapter.java
@@ -1,3 +1,5 @@
+package com.example.naviforyou;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
