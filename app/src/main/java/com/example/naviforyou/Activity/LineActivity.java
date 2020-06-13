1f2422d1fafab7837c86b8b5f36856ff3fc7f20d
ckdals4600 <ckdals4600@naver.com>
2020-06-13 15:42:00 +0900

    SCM 0613 Subway 목록 출력

--- app/src/main/java/com/example/naviforyou/Activity/LineActivity.java
+++ app/src/main/java/com/example/naviforyou/Activity/LineActivity.java
@@ -33,55 +33,55 @@ public class LineActivity extends AppCompatActivity {
         switch (line){
             case "1" :
                 line_name.setText("1호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line1, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
             case "2" :
                 line_name.setText("2호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line2, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
             case "3" :
                 line_name.setText("3호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line3, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
             case "4" :
                 line_name.setText("4호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line4, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
             case "5" :
                 line_name.setText("5호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line5, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
             case "6" :
                 line_name.setText("6호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line6, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
             case "7" :
                 line_name.setText("7호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line7, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
             case "8" :
                 line_name.setText("8호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line8, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
             case "9" :
                 line_name.setText("9호선");
-                arrayAdapter.createFromResource(this,
+                arrayAdapter = ArrayAdapter.createFromResource(this,
                         R.array.line9, android.R.layout.simple_list_item_1);
                 line_list.setAdapter(arrayAdapter);
                 break;
@@ -89,6 +89,7 @@ public class LineActivity extends AppCompatActivity {
 
         line_list.setOnItemClickListener((parent, view, position, id) -> {
             station = String.valueOf(parent.getItemIdAtPosition(position));
+
         });
     }
 }
